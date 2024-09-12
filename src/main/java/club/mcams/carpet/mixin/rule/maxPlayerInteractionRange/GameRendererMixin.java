cc6b798361b08d969172986c4388924ac3e5609c
1024_byteeeee <2270484921@qq.com>
2024-09-13 05:59:48 +0800

    :bug: 修复maxPlayerBlockInteractionRange、maxPlayerEntityInteractionRange规则不可用的问题

--- src/main/java/club/mcams/carpet/mixin/rule/maxPlayerInteractionRange/GameRendererMixin.java
+++ src/main/java/club/mcams/carpet/mixin/rule/maxPlayerInteractionRange/GameRendererMixin.java
@@ -3,14 +3,15 @@ package club.mcams.carpet.mixin.rule.maxPlayerInteractionRange;
 import club.mcams.carpet.AmsServerSettings;
 import club.mcams.carpet.helpers.rule.maxPlayerInteractionDistance_maxClientInteractionReachDistance.MaxInteractionDistanceMathHelper;
 
+import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
+
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.render.GameRenderer;
 
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
-import org.spongepowered.asm.mixin.injection.Constant;
-import org.spongepowered.asm.mixin.injection.ModifyConstant;
+import org.spongepowered.asm.mixin.injection.At;
 
 @Mixin(GameRenderer.class)
 public abstract class GameRendererMixin {
@@ -19,12 +20,7 @@ public abstract class GameRendererMixin {
     @Final
     private MinecraftClient client;
 
-    @ModifyConstant(
-        method = "updateTargetedEntity",
-        require = 0,
-        allow = 1,
-        constant = @Constant(doubleValue = 6.0D)
-    )
+    @ModifyExpressionValue(method = "updateTargetedEntity", at = @At(value = "CONSTANT", args = "doubleValue=6.0"))
     private double updateTargetedEntity1(final double constant) {
         if (AmsServerSettings.maxPlayerEntityInteractionRange != -1.0D && this.client.player != null) {
             return MaxInteractionDistanceMathHelper.getMaxSquaredReachDistance(AmsServerSettings.maxPlayerEntityInteractionRange);
@@ -32,7 +28,7 @@ public abstract class GameRendererMixin {
         return constant;
     }
 
-    @ModifyConstant(method = "updateTargetedEntity", constant = @Constant(doubleValue = 3.0D), require = 0)
+    @ModifyExpressionValue(method = "updateTargetedEntity", at = @At(value = "CONSTANT", args = "doubleValue=3.0"))
     private double updateTargetedEntity2(final double constant) {
         if (AmsServerSettings.maxPlayerEntityInteractionRange != -1.0D && this.client.player != null) {
             return MaxInteractionDistanceMathHelper.getMaxSquaredReachDistance(AmsServerSettings.maxPlayerEntityInteractionRange);
@@ -41,11 +37,7 @@ public abstract class GameRendererMixin {
         }
     }
 
-    @ModifyConstant(
-        method = "updateTargetedEntity",
-        require = 0,
-        constant = @Constant(doubleValue = 9.0D)
-    )
+    @ModifyExpressionValue(method = "updateTargetedEntity", at = @At(value = "CONSTANT", args = "doubleValue=9.0"))
     private double updateTargetedEntity3(final double constant) {
         if (AmsServerSettings.maxPlayerEntityInteractionRange != -1.0D && this.client.player != null) {
             return MaxInteractionDistanceMathHelper.getMaxSquaredReachDistance(AmsServerSettings.maxPlayerEntityInteractionRange);
--- src/main/java/club/mcams/carpet/mixin/rule/maxPlayerInteractionRange/ServerPlayNetworkHandlerMixin.java
+++ src/main/java/club/mcams/carpet/mixin/rule/maxPlayerInteractionRange/ServerPlayNetworkHandlerMixin.java
@@ -3,26 +3,16 @@ package club.mcams.carpet.mixin.rule.maxPlayerInteractionRange;
 import club.mcams.carpet.AmsServerSettings;
 import club.mcams.carpet.helpers.rule.maxPlayerInteractionDistance_maxClientInteractionReachDistance.MaxInteractionDistanceMathHelper;
 
+import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
+
 import net.minecraft.server.network.ServerPlayNetworkHandler;
-import net.minecraft.server.network.ServerPlayerEntity;
 
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
-import org.spongepowered.asm.mixin.injection.Constant;
-import org.spongepowered.asm.mixin.injection.ModifyConstant;
+import org.spongepowered.asm.mixin.injection.At;
 
 @Mixin(ServerPlayNetworkHandler.class)
 public abstract class ServerPlayNetworkHandlerMixin {
-
-    @Shadow
-    public ServerPlayerEntity player;
-
-    @ModifyConstant(
-        method = "onPlayerInteractBlock",
-        require = 0,
-        allow = 1,
-        constant = @Constant(doubleValue = 64.0)
-    )
+    @ModifyExpressionValue(method = "onPlayerInteractBlock", at = @At(value = "CONSTANT", args = "doubleValue=64.0"))
     private double onPlayerInteractBlock1(double constant) {
         if (AmsServerSettings.maxPlayerBlockInteractionRange != -1.0D) {
             return MaxInteractionDistanceMathHelper.getMaxSquaredReachDistance(AmsServerSettings.maxPlayerBlockInteractionRange);
@@ -31,12 +21,7 @@ public abstract class ServerPlayNetworkHandlerMixin {
         }
     }
 
-    @ModifyConstant(
-        method = "onPlayerInteractEntity",
-        require = 0,
-        allow = 2,
-        constant = @Constant(doubleValue = 36.0)
-    )
+    @ModifyExpressionValue(method = "onPlayerInteractEntity", at = @At(value = "CONSTANT", args = "doubleValue=36.0"))
     private double onPlayerInteractEntity(double constant) {
         if (AmsServerSettings.maxPlayerEntityInteractionRange != -1.0D) {
             return MaxInteractionDistanceMathHelper.getMaxSquaredReachDistance(AmsServerSettings.maxPlayerEntityInteractionRange);
